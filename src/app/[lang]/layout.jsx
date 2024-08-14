9bada9ea17778a982e4bc40af2c19f18eeae6c2e
unknown <blackdev0302@gmail.com>
2024-08-14 03:13:09 +0300

    title update

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -19,8 +19,8 @@ import '@/app/globals.css'
 import '@assets/iconify-icons/generated-icons.css'
 
 export const metadata = {
-  title: 'Materialize - Material Next.js Admin Template',
-  description: 'Materialize - Material Next.js Admin Template'
+  title: 'QNerp',
+  description: 'QNerp System'
 }
 
 const RootLayout = ({ children, params }) => {
